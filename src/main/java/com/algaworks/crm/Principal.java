bb6dc72baa43c0d6b3cedd90cd33547a8375c233
igor-lourenco <igor.lourencosantos@gmail.com>
2025-09-18 10:28:04 -0300

    Usando o Java Logging API (JUL)

--- src/main/java/com/algaworks/crm/Principal.java
+++ src/main/java/com/algaworks/crm/Principal.java
@@ -3,10 +3,15 @@ package com.algaworks.crm;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Principal {
 
+    private static final Logger log = Logger.getLogger(Principal.class.getName());
+
     public static void main(String[] args) {
+        log.info("Iniciando a classe main...");
 
         CadastroCliente cadastroCliente = new CadastroCliente();
 
@@ -16,11 +21,16 @@ public class Principal {
         cadastroCliente.adicionar(new Cliente("Joaquina de Jesus", 45));
         cadastroCliente.adicionar(new Cliente("Josefina Amaral", 25));
 
+
+        log.info("Quantidade de clientes adicionados: " + cadastroCliente.getClientes().size());
+
         List<Cliente> clientes = cadastroCliente.consultar(c -> c.getIdade() > 40);
 
         for (Cliente cliente : clientes) {
-            System.out.printf("%s - %d%n", StringUtils.abbreviate(cliente.getNome(), 15), cliente.getIdade());
+            log.log(Level.INFO, "{0} - {1} anos", new Object[]{
+                StringUtils.abbreviate(cliente.getNome(), 15), cliente.getIdade()});
         }
 
+        log.info("Fim :-)");
     }
 }
